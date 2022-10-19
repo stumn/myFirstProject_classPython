90b04017861b738a3c301430a21773121cb3fc83
stumn <pukut0317@gmail.com>
2022-10-19 14:43:46 +0900

    initial commitment

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    name = input("What is your name?")
-    print_hi(name)
+    # name = input("What is your name?")
+    print_hi('nakano')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
